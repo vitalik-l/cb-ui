61b98ab8a9be58660dddbb650bb887f6bf252892
Vitaly <mirenrich@gmail.com>
2019-07-05 14:46:51 +0300

    improve Modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Animate from 'rc-animate';
+import classNames from 'classnames';
 
 export default function Modal(props) {
-    const {children, open} = props;
+    const {children, open, overlayClassName, className} = props;
     return (
         <Animate transitionName="modal" transitionAppear>
             {open ?
-                <div className="overlay">
+                <div className={classNames('overlay', overlayClassName)}>
                     <div className="modal-wrapper">
-                        <div className="modal">
+                        <div className={classNames('modal', className)}>
                             {children}
                         </div>
                     </div>
