e0a0f0ad7d595ebb7f9c81931d2c315928a6e176
Vitaly <mirenrich@yandex.ru>
2020-05-14 11:30:06 +0300

    update modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -4,11 +4,11 @@ import CloseIcon from '../Icons/CloseIcon';
 import classNames from 'classnames';
 
 export default function Modal(props) {
-    const {children, open, overlayClassName, className, displayCloseButton, onClose} = props;
+    const {children, open, overlayClassName, className, displayCloseButton, onClose, onOverlayClick} = props;
     return (
         <Animate transitionName="modal" transitionAppear>
             {open ?
-                <div className={classNames('cb-Modal-overlay', overlayClassName)}>
+                <div className={classNames('cb-Modal-overlay', overlayClassName)} onClick={onOverlayClick}>
                     <div className="cb-Modal-wrapper">
                         <div className={classNames('cb-Modal', className)}>
                             {children}
