206fb4d85b249e4941f917c995537de0f40c64a9
Vitaly <mirenrich@yandex.ru>
2020-05-22 17:54:28 +0300

    update modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -3,6 +3,11 @@ import Animate from 'rc-animate';
 import CloseIcon from '../Icons/CloseIcon';
 import classNames from 'classnames';
 
+function onModalClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
 export default function Modal(props) {
     const {children, open, overlayClassName, className, displayCloseButton, onClose, onOverlayClick} = props;
     return (
@@ -10,7 +15,7 @@ export default function Modal(props) {
             {open ?
                 <div className={classNames('cb-Modal-overlay', overlayClassName)} onClick={onOverlayClick}>
                     <div className="cb-Modal-wrapper">
-                        <div className={classNames('cb-Modal', className)}>
+                        <div className={classNames('cb-Modal', className)} onclick={onModalClick}>
                             {children}
                             {displayCloseButton ?
                                 <div className="cb-Modal__close" onClick={onClose}>
