ffb0178841f15b48ba79be58cb2510b4d049d6b6
Vitaly <mirenrich@yandex.ru>
2020-07-21 16:19:26 +0300

    Button as example component

---
+++ src/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+    return (
+        <button className="cb-Button" {...props}>Test button</button>
+    );
+}
+
+Button.propTypes = {
+	/** Click handler */
+	onClick: PropTypes.func
+};
+
+export default Button
--- src/Button/story/Button.story.jsx
+++ src/Button/story/Button.story.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 
 const stories = storiesOf('Button', module);
 stories.addDecorator(withKnobs);
+stories.addDecorator(withInfo);
 
 // styles
 // import '../styles/style.scss';
 
-import Button from '../';
+import Button from '../index';
 
 stories.add('default', () => (
-    <Button />
+	<div className="story-Button">
+    	<Button />
+	</div>
 ));
 
 
