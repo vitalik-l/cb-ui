cc644a5359978dde4bece53730d317c9df65e20b
Vitaly <mirenrich@yandex.ru>
2020-07-21 14:33:37 +0300

    ButtonGroup component

---
+++ src/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,37 @@
+import React, {memo} from 'react';
+import PropTypes from 'prop-types';
+import omit from '../utils/omit';
+
+const ButtonGroup = memo(props => {
+    const {children, value, className} = props;
+
+    function onClick(e) {
+        const clickedValue = e.currentTarget.getAttribute('data-value');
+        if (clickedValue == null) return;
+        props.onChange && props.onChange(clickedValue, e);
+    }
+
+    const childrenItems = React.Children.map(children, (child) => {
+        let childProps = {};
+        if (value && !child.props.hasOwnProperty('active')) childProps.active = child.props.value == value;
+        if (props.onChange && !child.props.onClick) childProps.onClick = onClick;
+        return React.cloneElement(child, childProps);
+    });
+
+    return (
+        <ul className={className || 'cb-ButtonGroup'} {...omit(props, ['children', 'value', 'onChange'])}>
+            {childrenItems}
+        </ul>
+    )
+});
+
+ButtonGroup.propTypes = {
+    value: PropTypes.any,
+    children: PropTypes.any,
+    className: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+ButtonGroup.displayName = 'ButtonGroup';
+
+export default ButtonGroup;
---
+++ src/ButtonGroup/ButtonGroupItem.js
@@ -0,0 +1,24 @@
+import React, {memo} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import omit from '../utils/omit';
+
+const ButtonGroupItem = memo(props => {
+    const {children, active, value, className} = props;
+
+    return (
+        <li className={classNames(className, {active})} data-value={value} {...omit(props, ['className', 'value', 'children', 'active'])}>
+            {children}
+        </li>
+    );
+});
+
+ButtonGroupItem.propTypes = {
+    children: PropTypes.any,
+    active: PropTypes.bool,
+    value: PropTypes.any
+};
+
+ButtonGroupItem.displayName = 'ButtonGroupItem';
+
+export default ButtonGroupItem;
