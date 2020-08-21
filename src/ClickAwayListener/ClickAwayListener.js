9f904a5c8533407367d65da4dde226ae7be07b7b
Vitaly <mirenrich@yandex.ru>
2020-08-21 15:30:23 +0300

    add ClickAwayListener component

---
+++ src/ClickAwayListener/ClickAwayListener.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ClickAwayListener({ onClickAway, mouseEvent, children }) {
+  const childrenRef = React.useRef();
+  const eventName = React.useMemo(() => mouseEvent.substring(2).toLowerCase(), [mouseEvent]);
+
+  const childrenPropsHandler = React.useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const handler = children.props[mouseEvent];
+    if (handler) {
+      handler(e);
+    }
+  }, [children.props, mouseEvent]);
+
+  const childrenProps = {
+    ref: childrenRef,
+    [mouseEvent]: childrenPropsHandler,
+  };
+
+  const clickAwayListener = React.useCallback((e) => {
+    if (e.defaultPrevented) return;
+    onClickAway(e);
+  }, [onClickAway]);
+
+  React.useEffect(() => {
+    if (eventName) {
+      document.addEventListener(eventName, clickAwayListener);
+
+      return () => {
+        document.removeEventListener(eventName, clickAwayListener);
+      };
+    }
+
+    return undefined;
+  }, [eventName, clickAwayListener]);
+
+  return <React.Fragment>{React.cloneElement(children, childrenProps)}</React.Fragment>;
+}
+
+ClickAwayListener.propTypes = {
+  onClickAway: PropTypes.func.isRequired,
+  mouseEvent: PropTypes.string,
+  children: PropTypes.node,
+};
+
+ClickAwayListener.defaultProps = {
+  mouseEvent: 'onMouseDown',
+};
+
+export default ClickAwayListener;
---
+++ src/ClickAwayListener/story/ClickAwayListener.story.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import {
+  withKnobs,
+} from '@storybook/addon-knobs';
+import classNames from 'classnames';
+
+import ClickAwayListener from '..';
+
+const stories = storiesOf('ClickAwayListener', module);
+stories.addDecorator(withKnobs);
+
+stories.add('default', () => (
+  <div className={classNames('story-ClickAwayListener')}>
+    <ClickAwayListener
+      onClickAway={action('click away')}
+    >
+      <div>
+        See action by clicking away
+      </div>
+    </ClickAwayListener>
+  </div>
+));
