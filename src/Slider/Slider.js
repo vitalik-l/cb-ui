6b422a11ca8844041962802877ceb177c69cdfa8
Vitaly <mirenrich@yandex.ru>
2020-11-10 18:17:14 +0300

    slider

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -99,6 +99,26 @@ const Slider = React.forwardRef((props, ref) => {
       onTouchStart={handleAndStartDrag}
     >
       <div className="cb-Slider__fill" style={{width: fillWidth}} />
+      <div className="cb-Slider__handle">
+        <svg width="27" height="28" viewBox="0 0 27 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="1" y="1.5" width="25" height="25" rx="6" fill="#C4C4C4" stroke="#222324" strokeWidth="2"/>
+          <rect x="12" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="12" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="12" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="12" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="12" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="8" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="8" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="8" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="8" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="8" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="16" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="16" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="16" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="16" y="9.5" width="2" height="10" fill="#222324"/>
+          <rect x="16" y="9.5" width="2" height="10" fill="#222324"/>
+        </svg>
+      </div>
     </div>
   )
 });
