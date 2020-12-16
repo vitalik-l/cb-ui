aaed307c535ae9abfcf0046fc93066e64980a34f
Vitaly <mirenrich@yandex.ru>
2020-12-16 12:56:29 +0300

    release v1.1.23

--- packages/core/scripts/copy-styles.js
+++ packages/core/scripts/copy-styles.js
@@ -4,7 +4,20 @@ import fse from 'fs-extra';
 const SRC_DIR = './src';
 const finder = require('findit')(SRC_DIR);
 
-finder.on('file', function (file, stat) {
+// copy img and fonts folder
+finder.on('directory', function (dir, stat, stop) {
+  const dirname = path.parse(dir).name;
+  if (dirname === 'img' || dirname === 'fonts') {
+    const buildPath = dir.replace('src', 'lib');
+    fse.copy(dir, buildPath)
+      .then(() => console.log(`Copied ${dir} to ${buildPath}`))
+      .catch(err => console.error(err));
+    stop();
+  }
+});
+
+// copy scss files
+finder.on('file', function (file) {
   const fileParams = path.parse(file);
   const { dir, ext } = fileParams;
   if (ext === '.scss') {
