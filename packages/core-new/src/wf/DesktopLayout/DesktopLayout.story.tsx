f6af20771307666861ebe2bc68246fabaf72f8f0
Vitaly <mirenrich@yandex.ru>
2021-02-19 17:34:06 +0300

    wf: update DesktopLayoutStory

--- packages/core-new/src/wf/DesktopLayout/DesktopLayout.story.tsx
+++ packages/core-new/src/wf/DesktopLayout/DesktopLayout.story.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { Root } from '@cb-general/core/Root';
-import { Tab, Tabs } from '@cb-general/weekend/Tabs';
-import { Panel } from '@cb-general/weekend/Panel';
 
 // local files
 import { createStory } from '../createStory';
@@ -12,6 +10,7 @@ import { TickerTemplate } from '../../weekend/Ticker/Ticker.story';
 import { ChartControlsTemplate } from '../../weekend/ChartControls/ChartControls.story';
 import { TradingControlsTemplate } from '../TradingControls/TradingControls.story';
 import { Logo } from '../Logo';
+import { TabsPanelTemplate } from '../../weekend/TabsPanel/TansPanel.story';
 
 const story = createStory({
   title: 'DesktopLayout',
@@ -25,15 +24,7 @@ export const Template: Story = (args) => (
       header={<HeaderTemplate logo={<Logo />} />}
       ticker={<TickerTemplate />}
       chartControls={<ChartControlsTemplate />}
-      tabs={
-        <Panel>
-          <Tabs value={0}>
-            <Tab label="Open Orders" />
-            <Tab label="Closed Orders" />
-            <Tab label="Settings" />
-          </Tabs>
-        </Panel>
-      }
+      tabs={<TabsPanelTemplate />}
       tradingControls={<TradingControlsTemplate />}
     />
   </Root>
