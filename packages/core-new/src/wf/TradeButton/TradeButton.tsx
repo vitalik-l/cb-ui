5030ef8988dc324fdbda93cc080ef57a20c8550c
Vitaly <mirenrich@yandex.ru>
2021-02-26 11:54:07 +0300

    wf: added animation styles

--- packages/core-new/src/wf/TradeButton/TradeButton.tsx
+++ packages/core-new/src/wf/TradeButton/TradeButton.tsx
@@ -10,17 +10,25 @@ type Props = {
   className?: string;
   children?: React.ReactNode;
   color?: 'default' | 'green' | 'red';
+  active?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const TradeButton = (props: Props) => {
-  const { className, color = 'default', ...restProps } = props;
+  const { className, color = 'default', active = true, disabled: disabledProp, ...restProps } = props;
 
   return (
     <ButtonBase
       className={clsx(classes.TradeButton, className, {
+        [`${classes.TradeButton}_colored`]: color !== 'default',
         [`${classes.TradeButton}_color_${color}`]: !!color,
+        [`${classes.TradeButton}_inactive`]: !active,
       })}
+      disabled={disabledProp || !active}
       {...restProps}
     />
   );
 };
+
+TradeButton.defaultProps = {
+  active: true,
+}
--- packages/core-new/src/wf/styles/Styles.story.tsx
+++ packages/core-new/src/wf/styles/Styles.story.tsx
@@ -11,13 +11,19 @@ const story = createStory({
 
 // uncomment next line
 export const Default: Story = () => (
-  <>
-    <h3 className="text-white">colors</h3>
-    <div className="text-green">text-green</div>
-    <div className="text-red">text-red</div>
-    <div className="text-yellow">text-yellow</div>
-    <div className="text-orange">text-orange</div>
-  </>
+  <div>
+    <div>
+      <h3 className="text-white">Colors</h3>
+      <div className="text-green">text-green</div>
+      <div className="text-red">text-red</div>
+      <div className="text-yellow">text-yellow</div>
+      <div className="text-orange">text-orange</div>
+    </div>
+    <div>
+      <h3 className="text-white">Animations</h3>
+      <div className="animate-in-trade text-green">animate-in-trade</div>
+    </div>
+  </div>
 );
 Default.storyName = 'Styles';
 
