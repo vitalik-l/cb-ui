abdf3be85fc21756ae8f36dcd94b8532652c79e9
Vitaly <mirenrich@yandex.ru>
2021-03-18 15:11:59 +0300

    wf: mobile components

--- packages/core-new/src/wf/mobile/createStory.tsx
+++ packages/core-new/src/wf/mobile/createStory.tsx
@@ -1,9 +1,10 @@
 import { createStoryFactory } from '../../core/createStoryFactory';
+import classes from './styles/classes.module.scss';
 
 const { createStory } = createStoryFactory({
   titlePrefix: 'wf/mobile/',
   defaultStyle: { fontSize: '1rem' },
-  className: 'WfMobileStory',
+  className: classes.Story,
 });
 
 export { createStory };
