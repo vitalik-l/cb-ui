7b415756aa307bf76cc6b925ea05c953413d436e
Vitaly <mirenrich@yandex.ru>
2021-03-18 11:57:59 +0300

    flat: added Header

---
+++ packages/core-new/src/flat/Header/Header.story.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+
+// local files
+import { createStory } from '../createStory';
+import { Header } from './index';
+
+const story = createStory({
+  title: 'Header',
+  component: Header,
+  style: {
+    background: '#000',
+  }
+});
+
+export const Template: Story = () => (
+  <Header
+    leftContent={<div>Left Content</div>}
+    centerContent={<div>Center Content</div>}
+    rightContent={<div>Right Content</div>}
+  />
+);
+Template.storyName = 'Header';
+
+export default story;
