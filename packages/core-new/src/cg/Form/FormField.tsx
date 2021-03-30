4a98a1159df638eaa27bd8766f9affa90374af58
Vitaly <mirenrich@yandex.ru>
2021-03-30 11:35:53 +0300

    cg: components

--- packages/core-new/src/cg/Form/FormField.tsx
+++ packages/core-new/src/cg/Form/FormField.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { FormField as CoreFormField } from '@cb-general/core/Form';
 
 // local files
-import { Label } from './Label';
+import { Label } from '../Label';
+import { Input } from '../Input';
 import styles from './CgFormField.module.scss';
 
-export const FormField = (props: any) => <CoreFormField {...props} Label={Label} classes={styles} />;
+export const FormField = (props: any) => <CoreFormField component={Input} {...props} Label={Label} classes={styles} />;
