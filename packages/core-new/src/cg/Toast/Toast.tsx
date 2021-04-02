ea37ff102bc0223f3038687ee307ee4408853685
Vitaly <mirenrich@yandex.ru>
2021-04-02 15:37:02 +0300

    added Toast components

---
+++ packages/core-new/src/cg/Toast/Toast.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { styled } from '@cb-general/core/utils/styled';
+import { Toast as CoreToast } from '@cb-general/core/Toast';
+// local files
+import styles from './CgToast.module.scss';
+
+export const Toast = styled(CoreToast, styles);
---
+++ packages/core-new/src/core/Toast/Toast.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import clsx from 'clsx';
+import Animate from 'rc-animate';
+
+// local files
+import styles from './CoreToast.module.scss';
+import {useClasses} from '../hooks/useClasses';
+
+type ClassesType = {
+  root?: string;
+  content?: string;
+  [key: string]: any;
+}
+
+type Props = {
+  className?: string;
+  children?: React.ReactNode;
+  text?: string;
+  color?: 'default' | 'success' | 'danger' | 'warning' | 'info';
+  placementX?: 'left' | 'center' | 'right';
+  placementY?: 'top' | 'bottom' | 'center';
+  classes?: ClassesType;
+  transitionName?: string;
+} & React.ComponentProps<'div'>;
+
+export const Toast = (props: Props) => {
+  const { className, children, text, color = 'default', placementX, placementY, classes: classesProp, transitionName, ...restProps } = props;
+  const content = children || text;
+  const classes: ClassesType = useClasses(styles, classesProp);
+
+  return (
+    <Animate transitionName={styles.animate || transitionName} transitionAppear>
+      {!!content && (
+        <div
+          className={clsx(
+            classes.root,
+            className,
+            classes[`vertical_${placementY}`],
+            classes[`horizontal_${placementX}`],
+          )}
+          key={text || ''}
+        >
+          <div
+            className={clsx(classes.content, !!color && classes[`color_${color}`])}
+            {...restProps}
+          >
+            {content}
+          </div>
+        </div>
+      )}
+    </Animate>
+  );
+};
+
+Toast.defaultProps = {
+  placementX: 'center',
+  placementY: 'top',
+};
