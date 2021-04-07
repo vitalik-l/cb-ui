ae767ab0fa9732557b566c5280e28bde4f3c708e
Vitaly <mirenrich@yandex.ru>
2021-04-07 16:44:25 +0300

    come components improvements

--- packages/core-new/src/cg/Form/ClassicFormField.tsx
+++ packages/core-new/src/cg/Form/ClassicFormField.tsx
@@ -4,7 +4,7 @@ import { FormField as CoreFormField } from '@cb-general/core/Form';
 // local files
 import { ClassicLabel } from '../Label';
 import { ClassicInput } from '../Input';
-import styles from './CgFormField.module.scss';
+import styles from './ClassicFormField.module.scss';
 
 export const ClassicFormField = (props: any) => (
   <CoreFormField component={ClassicInput} {...props} Label={ClassicLabel} classes={styles} />
