bf5c77dd841566961f799b9228b64c0593ec7351
Vitaly <mirenrich@yandex.ru>
2021-04-14 16:48:35 +0300

    weekend: refactor

--- packages/core-new/src/weekend/Select/Select.tsx
+++ packages/core-new/src/weekend/Select/Select.tsx
@@ -23,12 +23,7 @@ export const Select = React.forwardRef((props: Props, ref: any) => {
       ref={ref}
     >
       <select className={styles.select} disabled={disabled} {...selectOptions} />
-      <svg
-        className={styles.icon}
-        focusable="false"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-      >
+      <svg className={styles.icon} focusable="false" viewBox="0 0 24 24" aria-hidden="true">
         <path d="M7 10l5 5 5-5z" />
       </svg>
     </div>
