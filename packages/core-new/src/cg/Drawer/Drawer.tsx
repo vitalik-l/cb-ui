a04111e599cb63ab3d7a553f15fc2efabec1a902
Vitaly <mirenrich@yandex.ru>
2021-04-15 14:07:04 +0300

    cg: update Drawer

--- packages/core-new/src/cg/Drawer/Drawer.tsx
+++ packages/core-new/src/cg/Drawer/Drawer.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
+import { useClasses } from '@cb-general/core/hooks/useClasses';
 
 // local files
 import styles from './CgDrawer.module.scss';
 
+type ClassesType = {
+  root?: string;
+  open?: string;
+  [key: string]: any;
+};
+
 export type DrawerProps = {
   open?: boolean;
+  classes?: ClassesType;
+  appear?: 'left' | 'right' | 'bottom' | 'top';
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export const Drawer = (props: DrawerProps) => {
-  const { className, open, ...restProps } = props;
+  const { className, open, classes: classesProp, appear, ...restProps } = props;
+  const classes: ClassesType = useClasses(styles, classesProp);
 
-  return <div className={clsx(styles.root, className, { [styles.open]: open })} {...restProps} />;
+  return (
+    <div
+      className={clsx(
+        classes.root,
+        className,
+        appear && classes[`appear_${appear}`],
+        open && classes.open,
+      )}
+      {...restProps}
+    />
+  );
+};
+
+Drawer.defaultProps = {
+  appear: 'right',
 };
