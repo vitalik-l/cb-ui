81c09369273300183e240cb20ea49e74557a2a96
Vitaly <mirenrich@yandex.ru>
2021-04-21 17:24:49 +0300

    refactoring

--- packages/ui/src/wf/src/DesktopLayout/DesktopLayout.story.tsx
+++ packages/ui/src/wf/src/DesktopLayout/DesktopLayout.story.tsx
@@ -5,12 +5,12 @@ import { Root } from '@cb-general/core/Root';
 // local files
 import { createStory } from '../../story';
 import { DesktopLayout } from './index';
-import { HeaderTemplate } from '../../../weekend/src/Header/Header.story';
-import { TickerTemplate } from '../../../weekend/src/Ticker/Ticker.story';
-import { ChartControlsTemplate } from '../../../weekend/src/ChartControls/ChartControls.story';
-import { TradingControlsTemplate } from '../TradingControls/TradingControls.story';
-import { Logo } from '../Logo';
-import { TabsPanelTemplate } from '../../../weekend/src/TabsPanel/TansPanel.story';
+// import { HeaderTemplate } from '../../../weekend/src/Header/Header.story';
+// import { TickerTemplate } from '../../../weekend/src/Ticker/Ticker.story';
+// import { ChartControlsTemplate } from '../../../weekend/src/ChartControls/ChartControls.story';
+// import { TradingControlsTemplate } from '../TradingControls/TradingControls.story';
+// import { Logo } from '../Logo';
+// import { TabsPanelTemplate } from '../../../weekend/src/TabsPanel/TansPanel.story';
 
 const story = createStory({
   title: 'DesktopLayout',
@@ -21,11 +21,6 @@ export const Template: Story = (args) => (
   <Root>
     <DesktopLayout
       {...args}
-      header={<HeaderTemplate logo={<Logo />} />}
-      ticker={<TickerTemplate />}
-      chartControls={<ChartControlsTemplate />}
-      tabs={<TabsPanelTemplate />}
-      tradingControls={<TradingControlsTemplate />}
     />
   </Root>
 );
