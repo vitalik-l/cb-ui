2f5ece2f9efa7f4bb51048359e45a16b25fe979b
Vitaly <mirenrich@yandex.ru>
2021-04-22 18:02:48 +0300

    refactoring

--- packages/ui/src/flat/src/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { NavIcon } from './NavIcon';
-export { CloseIcon } from './CloseIcon';
-export { TrendIcon } from './TrendIcon';
-export { CircleMenuIcon } from './CircleMenuIcon';
-export { MenuIcon } from './MenuIcon';
-export { SelectIcon } from './SelectIcon';
