6342afad99f0483a731533c918648722e44232a6
Vitaly <mirenrich@yandex.ru>
2021-04-23 12:32:01 +0300

    rfx: RouletteWheel component

--- packages/ui/src/rfx/src/RouletteWheel/RouletteWheel.tsx
+++ packages/ui/src/rfx/src/RouletteWheel/RouletteWheel.tsx
@@ -6,10 +6,20 @@ import styles from './RouletteWheel.module.scss';
 
 type Props = {
   className?: string;
+  slots?: 'redblack';
 };
 
 export const RouletteWheel = (props: Props) => {
-  const { className } = props;
+  const { className, slots } = props;
 
-  return <div className={clsx(styles.root, className)} />;
+  return (
+    <div className={clsx(styles.root, className)}>
+      <div className={styles.bg} />
+      <div className={clsx(styles.slots, styles[`slots_${slots}`])} />
+    </div>
+  );
 };
+
+RouletteWheel.defaultProps = {
+  slots: 'redblack',
+}
