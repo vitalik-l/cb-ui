c208ed1a303c34fe36d926c3953caa086da694bd
Vitaly <mirenrich@yandex.ru>
2021-04-26 13:31:21 +0300

    rfx: GameButton component

--- packages/ui/src/weekend/src/GameButton/GameButton.tsx
+++ packages/ui/src/weekend/src/GameButton/GameButton.tsx
@@ -3,17 +3,19 @@ import clsx from 'clsx';
 import { ButtonBase } from '@cb-general/core/ButtonBase';
 
 // local files
-import styles from './GameButton.module.scss';
+import styles from './WkdGameButton.module.scss';
 
 type Props = React.ComponentProps<typeof ButtonBase> & {
   color?: 'primary' | 'default';
+  sublabel?: string;
 };
 
 export const GameButton = (props: Props) => {
-  const { className, color, children, ...restProps } = props;
+  const { className, color, children, sublabel, ...restProps } = props;
 
   return (
     <ButtonBase className={clsx(styles.root, className, color && styles[`color_${color}`])} {...restProps}>
+      <span className={styles.sublabel}>{sublabel}</span>
       <span className={styles.label}>{children}</span>
     </ButtonBase>
   );
