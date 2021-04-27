c8e62a5ef2c1b972aa8a6c2ebd74bd4bec51374a
Vitaly <mirenrich@yandex.ru>
2021-04-27 13:56:44 +0300

    weekend: ChipsStack component

--- packages/ui/src/rfx/src/Dropzones/Dropzone.tsx
+++ packages/ui/src/rfx/src/Dropzones/Dropzone.tsx
@@ -12,12 +12,13 @@ type Props = React.ComponentProps<typeof ButtonBase> & {
 };
 
 export const Dropzone = (props: Props) => {
-  const { className, label, roi, type, ...restProps } = props;
+  const { className, label, roi, type, children, ...restProps } = props;
 
   return (
     <ButtonBase className={clsx(styles.dropzone, className, !!type && styles[type])} {...restProps}>
       {!!label && <div className={styles.label}>{label}</div>}
       {!!roi && <div className={styles.roi}>{roi}</div>}
+      {children}
     </ButtonBase>
   );
 };
--- packages/ui/src/rfx/src/Dropzones/Dropzones.story.tsx
+++ packages/ui/src/rfx/src/Dropzones/Dropzones.story.tsx
@@ -4,6 +4,8 @@ import { Story } from '@storybook/react';
 // local files
 import { createStory } from '../../story';
 import { Dropzones, Dropzone } from './index';
+import { ChipsStack } from '../../../weekend/src/ChipsStack';
+import { Chip } from '../../../weekend/src/Chip';
 
 const story = createStory({
   title: 'Dropzones',
@@ -12,8 +14,28 @@ const story = createStory({
 
 export const DropzonesTemplate: Story = (args) => (
   <Dropzones {...args}>
-    <Dropzone type="down" label="RED" roi="1:1" />
-    <Dropzone type="up" label="BLACK" roi="1:1" />
+    <Dropzone type="down" label="RED" roi="1:1">
+      <ChipsStack>
+        <Chip />
+        <Chip />
+      </ChipsStack>
+      <ChipsStack>
+        <Chip />
+        <Chip />
+        <Chip />
+      </ChipsStack>
+    </Dropzone>
+    <Dropzone type="up" label="BLACK" roi="1:1">
+      <ChipsStack>
+        <Chip />
+        <Chip />
+      </ChipsStack>
+      <ChipsStack>
+        <Chip />
+        <Chip />
+        <Chip />
+      </ChipsStack>
+    </Dropzone>
   </Dropzones>
 );
 DropzonesTemplate.storyName = 'Dropzones';
