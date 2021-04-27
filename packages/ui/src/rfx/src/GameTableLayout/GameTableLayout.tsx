30d884ae32fda8e9c18e94c05d50948a891768a6
Vitaly <mirenrich@yandex.ru>
2021-04-27 17:40:45 +0300

    rfx: GameTableLayout component

--- packages/ui/src/rfx/src/GameTableLayout/GameTableLayout.tsx
+++ packages/ui/src/rfx/src/GameTableLayout/GameTableLayout.tsx
@@ -3,16 +3,19 @@ import clsx from 'clsx';
 
 // local files
 import styles from './GameTableLayout.module.scss';
+import { ToggleTableButton } from '../ToggleTableButton';
 
 type Props = React.ComponentProps<'div'> & {
   rouletteWheel?: React.ReactElement;
   chips?: React.ReactElement;
   gameRounds?: React.ReactElement;
   buttons?: React.ReactElement;
+  hidden?: boolean;
+  onToggle?: any;
 };
 
 export const GameTableLayout = (props: Props) => {
-  const { className, rouletteWheel, chips, gameRounds, buttons, ...restProps } = props;
+  const { className, rouletteWheel, chips, gameRounds, buttons, hidden, onToggle, ...restProps } = props;
 
   return (
     <div className={clsx(styles.root, className)} {...restProps}>
@@ -24,6 +27,9 @@ export const GameTableLayout = (props: Props) => {
         {buttons}
       </div>
       {!!gameRounds && React.cloneElement(gameRounds, { className: styles.gameRounds })}
+      <div className={styles.toggleButton}>
+        <ToggleTableButton onClick={onToggle} hidden={hidden} />
+      </div>
     </div>
   );
 };
