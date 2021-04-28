0ce519d8dff9cc8be83f12ec5789eb1271bc9280
Vitaly <mirenrich@yandex.ru>
2021-04-28 22:57:11 +0300

    rfx: update dropzone

--- packages/ui/src/rfx/src/Dropzones/Dropzones.story.tsx
+++ packages/ui/src/rfx/src/Dropzones/Dropzones.story.tsx
@@ -6,7 +6,6 @@ import { createStory } from '../../story';
 import { Dropzones, Dropzone } from './index';
 import { ChipsStack } from '../../../weekend/src/ChipsStack';
 import { Chip } from '../../../weekend/src/Chip';
-import { InfoPanel, InfoPanelItem } from '../../../core/src/InfoPanel';
 
 const story = createStory({
   title: 'Dropzones',
@@ -22,11 +21,6 @@ export const DropzonesTemplate: Story = () => (
       type="down"
       label="RED"
       roi="1:1"
-      tooltip={
-        <InfoPanel>
-          <InfoPanelItem>Tooltip</InfoPanelItem>
-        </InfoPanel>
-      }
     >
       <ChipsStack label="$999,999,999">
         <Chip />
@@ -58,12 +52,6 @@ export const DropzonesTemplate: Story = () => (
       type="up"
       label="BLACK"
       roi="1:1"
-      tooltip={
-        <InfoPanel>
-          <InfoPanelItem>Tooltip Open</InfoPanelItem>
-        </InfoPanel>
-      }
-      tooltipOpen
     >
       <ChipsStack label="$999,999,999">
         <Chip />
