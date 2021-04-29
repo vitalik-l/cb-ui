b3fd146b2547438011775a8b589f4fdcc28ba051
Vitaly <mirenrich@yandex.ru>
2021-04-29 18:31:40 +0300

    weekend: added AutoplayCounter

--- packages/ui/src/weekend/src/AutoplayCounter/AutoplayCounter.tsx
+++ packages/ui/src/weekend/src/AutoplayCounter/AutoplayCounter.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import clsx from 'clsx';
-
-// local files
+import { styled } from '@cb-general/core/utils/styled';
 import styles from './AutoplayCounter.module.scss';
-
-type Props = React.ComponentProps<'div'>;
-
-export const AutoplayCounter = (props: Props) => {
-  const { className, ...restProps } = props;
-
-  return <div className={clsx(styles.root, className)} {...restProps} />;
-};
+export const AutoplayCounter = styled(styles.root);
