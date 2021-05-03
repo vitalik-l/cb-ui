0f9e535ba09e61d0edfbabbabd2cd2af077bac20
Vitaly <mirenrich@yandex.ru>
2021-05-03 22:00:11 +0300

    rfx: Dropzones update

--- packages/ui/src/rfx/src/Dropzones/Dropzones.tsx
+++ packages/ui/src/rfx/src/Dropzones/Dropzones.tsx
@@ -7,7 +7,12 @@ import styles from './Dropzones.module.scss';
 type Props = React.ComponentProps<'div'>;
 
 export const Dropzones = (props: Props) => {
-  const { className, ...restProps } = props;
+  const { className, children, ...restProps } = props;
 
-  return <div className={clsx(styles.root, className)} {...restProps} />;
+  return (
+    <div className={clsx(styles.root, className)} {...restProps}>
+      <div className={styles.center} />
+      {children}
+    </div>
+  );
 };
