ca8eb47652ec36f6f8e78ddca9fcde95bd54352d
Vitaly <mirenrich@yandex.ru>
2021-05-04 12:24:50 +0300

    rfx: Mobile

--- packages/ui/src/rfx/src/MobileChipsControls/MobileChipsControls.story.tsx
+++ packages/ui/src/rfx/src/MobileChipsControls/MobileChipsControls.story.tsx
@@ -4,13 +4,26 @@ import { Story } from '@storybook/react';
 // local files
 import { createStory } from '../../story';
 import { MobileChipsControls } from './index';
+import {Chip} from '../../../weekend/src/Chip';
 
 const story = createStory({
   title: 'MobileChipsControls',
   component: MobileChipsControls,
 });
 
-export const Template: Story = (args) => <MobileChipsControls {...args} />;
-Template.storyName = 'MobileChipsControls';
+export const MobileChipsControlsTemplate: Story = (args) => {
+  const [value, setValue] = React.useState(1);
+
+  return (
+    <MobileChipsControls {...args} value={value} onChange={setValue}>
+      <Chip value={1} large />
+      <Chip value={5} large />
+      <Chip value={10} large />
+      <Chip value={25} large />
+      <Chip value={100} large />
+    </MobileChipsControls>
+  );
+};
+MobileChipsControlsTemplate.storyName = 'MobileChipsControls';
 
 export default story;
