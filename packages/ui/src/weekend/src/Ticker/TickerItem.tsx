47f2521c99a3950c18a688f54924f1abf3ca0e75
Vitaly <mirenrich@yandex.ru>
2021-12-08 08:00:38 +0300

    improve TickerItem component

--- packages/ui/src/weekend/src/Ticker/TickerItem.tsx
+++ packages/ui/src/weekend/src/Ticker/TickerItem.tsx
@@ -6,13 +6,16 @@ type Props = {
   className?: string;
   children?: React.ReactNode;
   position?: 'left' | 'right';
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
 export const TickerItem = (props: Props) => {
-  const { className, children, position } = props;
+  const { className, children, position, ...restProps } = props;
 
   return (
-    <div className={clsx(styles.root, className, !!position && styles?.[`position_${position}`])}>
+    <div
+      className={clsx(styles.root, className, !!position && styles?.[`position_${position}`])}
+      {...restProps}
+    >
       {children}
     </div>
   );
