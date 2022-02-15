7ccaa66b6d554396062c2aa548403d4337024617
Vitaly <mirenrich@yandex.ru>
2022-02-15 15:14:24 +0300

    weekend: Chip updated

--- packages/ui/src/weekend/src/Chip/Chip.tsx
+++ packages/ui/src/weekend/src/Chip/Chip.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import clsx from 'clsx';
+import React from 'react';
 import { useClasses } from '@cb-general/core/hooks/useClasses';
-
-// local files
 import styles from './WkdChip.module.scss';
 
 type Props = {
@@ -14,6 +12,26 @@ type Props = {
   classes?: any;
   children?: string | number;
 } & React.HTMLAttributes<HTMLDivElement>;
+
+const nFormatter = (num: number | string, digits = 1) => {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'k' },
+    { value: 1e6, symbol: 'M' },
+    { value: 1e9, symbol: 'G' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'P' },
+    { value: 1e18, symbol: 'E' },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item ? (+num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol : '0';
+};
 
 export const Chip = React.forwardRef((props: Props, ref: any) => {
   const {
@@ -27,8 +45,7 @@ export const Chip = React.forwardRef((props: Props, ref: any) => {
     ...restProps
   } = props;
   const content = children || valueProp;
-  const value: number | string =
-    format && +content >= 1000 ? `${Math.round(+content / 1000)}K` : content;
+  const value: number | string = format && +content >= 1000 ? nFormatter(content) : content;
   const classes = useClasses(styles, classesProp);
 
   return (
