fabd7541f4ac2c823ecf3edb3fbee334c82a9550
Vitaly <mirenrich@yandex.ru>
2022-03-25 16:11:56 +0300

    cg: updated version

--- packages/ui/src/cg/src/GameLink/GameLink.tsx
+++ packages/ui/src/cg/src/GameLink/GameLink.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import clsx from 'clsx';
+import React from 'react';
 import { ButtonBase } from '@cb-general/core/ButtonBase';
-
-// local
-import styles from './CgGameLink.module.scss';
+import { useStyles } from '@cb-general/core/hooks';
+import baseStyles from './CgGameLink.module.scss';
 
 type Props = {
   game: 'wallstbaccarat' | 'roulettefx' | 'wallstroulette' | 'weekendforex' | 'weekendbinary';
   active?: boolean;
   className?: string;
+  styles?: any;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 export const GameLink = (props: Props) => {
-  const { className, game, active, ...restProps } = props;
+  const { className, game, active, styles: stylesProp, ...restProps } = props;
+  const styles = useStyles(baseStyles, stylesProp);
 
   return (
     <ButtonBase
