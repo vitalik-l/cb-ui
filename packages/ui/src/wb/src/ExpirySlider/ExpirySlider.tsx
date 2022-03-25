0a1b1b68dcd7283388a9534da83e28a1bdfdb18a
Vitaly <mirenrich@yandex.ru>
2022-03-25 14:16:25 +0300

    wb: updated version

--- packages/ui/src/wb/src/ExpirySlider/ExpirySlider.tsx
+++ packages/ui/src/wb/src/ExpirySlider/ExpirySlider.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import { Box } from '@cb-general/core/Box';
 import { ButtonBase } from '@cb-general/core/ButtonBase';
 import { ButtonGroup, ButtonGroupThumbProps } from '@cb-general/core/ButtonGroup';
+import { useStyles } from '@cb-general/core/hooks';
 import { styled } from '@cb-general/core/utils/styled';
-import styles from './WbExpirySlider.module.scss';
+import stylesBase from './WbExpirySlider.module.scss';
 
-const Thumb = ({ selectedIndex }: ButtonGroupThumbProps) => {
+const thumbStyles = {
+  root: stylesBase.thumb,
+  shadow: stylesBase.thumbShadow,
+};
+
+export const Thumb = ({
+  selectedIndex,
+  styles: stylesProp,
+}: ButtonGroupThumbProps & { styles: { root?: string; shadow?: string } }) => {
   const transform =
     selectedIndex !== undefined && selectedIndex > -1
-      ? `translateX(calc(${selectedIndex} * ${styles.itemWidth}))`
+      ? `translateX(calc(${selectedIndex} * ${stylesBase.itemWidth}))`
       : undefined;
+  const styles = useStyles(thumbStyles, stylesProp);
 
   return (
-    <Box className={styles.thumb} transform={transform}>
-      <div className={styles.thumbShadow} />
+    <Box className={styles.root} transform={transform}>
+      <div className={styles.shadow} />
     </Box>
   );
 };
 
-export const ExpirySliderItem = styled(ButtonBase, styles.item);
+export const ExpirySliderItem = styled(ButtonBase, stylesBase.item);
 
-export const ExpirySlider = styled(ButtonGroup, styles);
+export const ExpirySlider = styled(ButtonGroup, stylesBase);
 
 ExpirySlider.defaultProps = {
   ThumbComponent: Thumb,
