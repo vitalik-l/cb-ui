f707c6ae80102942d79cb88a69a8ef74f7e30ee9
Vitaly <mirenrich@yandex.ru>
2022-03-25 14:15:23 +0300

    weekend: updated version

--- packages/ui/src/weekend/src/ProgressButton/ProgressButton.tsx
+++ packages/ui/src/weekend/src/ProgressButton/ProgressButton.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
 import clsx from 'clsx';
-
-// local files
+import React from 'react';
+import { useStyles } from '@cb-general/core/hooks';
 import { Button } from '../Button';
-import styles from './WkdProgressButton.module.scss';
+import baseStyles from './WkdProgressButton.module.scss';
+
+type Colors = 'red' | 'green';
+type ColorClasses = { [key in `color_${Colors}`]?: string };
+
+type Styles = {
+  root?: string;
+  bg?: string;
+  progress?: string;
+} & ColorClasses;
 
 type Props = {
+  styles?: Styles;
   className?: string;
   disabled?: boolean;
-  color?: 'red' | 'green';
+  color?: Colors;
   label?: string;
   progress?: number;
 };
 
 export const ProgressButton = (props: Props) => {
-  const { className, color, label, progress, ...otherProps } = props;
+  const { className, color, label, progress, styles: stylesProp, ...otherProps } = props;
+  const styles = useStyles(baseStyles, stylesProp);
+  const colorClass = `color_${color}` as keyof ColorClasses;
 
   return (
     <Button className={clsx(styles.root, className)} data-label={label} square {...otherProps}>
       {label}
-      <span className={clsx(styles.bg, !!color && styles[`color_${color}`])} />
+      <span className={clsx(styles.bg, !!color && styles[colorClass])} />
       <span className={styles.progress} style={{ transform: `translateX(${progress}%)` }} />
     </Button>
   );
