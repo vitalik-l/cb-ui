2d98b6377495f3e3c2b02558eb24023e342a40cf
Vitaly <mirenrich@yandex.ru>
2023-07-13 14:50:57 +0300

    core: add form props to form

--- packages/ui/src/core/src/Form/Form.tsx
+++ packages/ui/src/core/src/Form/Form.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Form as FinalForm, FormProps } from 'react-final-form';
-
-// local files
-import { FieldsLayout } from './FieldsLayout';
-import { FormContext } from './FormContext';
+import React, {FormHTMLAttributes} from 'react';
+import {Form as FinalForm, FormProps} from 'react-final-form';
+import {FieldsLayout} from './FieldsLayout';
+import {FormContext} from './FormContext';
 
 type Props = {
   className?: string;
   layout?: 'default' | 'inline' | 'stacked';
   children?: any;
   fullWidth?: boolean;
+  htmlForm?: FormHTMLAttributes<any>
 } & FormProps;
 
 export const Form = React.forwardRef((props: Props, ref: any) => {
-  const { className, children, layout = 'default', id, fullWidth, ...formProps } = props;
+  const {className, children, layout = 'default', id, fullWidth, htmlForm, ...finalFormProps} = props;
   const context = React.useMemo(
     () => ({
       isCoreForm: true,
@@ -24,17 +23,17 @@ export const Form = React.forwardRef((props: Props, ref: any) => {
 
   return (
     <FormContext.Provider value={context}>
-      <FinalForm {...formProps}>
-        {({ handleSubmit, ...formState }) => (
-          <form className={className} onSubmit={handleSubmit} id={id} ref={ref}>
+      <FinalForm {...finalFormProps}>
+        {({handleSubmit, ...formState}) => (
+          <form className={className} onSubmit={handleSubmit} id={id} ref={ref} {...htmlForm}>
             {layout ? (
               <FieldsLayout type={layout}>
                 {typeof children === 'function'
-                  ? children({ handleSubmit, ...formState })
+                  ? children({handleSubmit, ...formState})
                   : children}
               </FieldsLayout>
             ) : typeof children === 'function' ? (
-              children({ handleSubmit, ...formState })
+              children({handleSubmit, ...formState})
             ) : (
               children
             )}
